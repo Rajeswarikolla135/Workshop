4433892b44fc3b9babeb27c23d1e4876864dc3a9
Rajeswarikolla135 <rajeswarikolla135@gmail.com>
2023-08-29 14:32:22 +0530

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * devoopsworkshop!!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "devoopsworkshop!!" );
+    }
+}
